ab2f8356efe3d3928875927602dedc6e3e9b5798
Rodrigo Lemes <rodrigo.lemes8@icloud.com>
2023-06-01 15:17:11 -0300

    Evitando Tipagens

---
+++ Javascript/Typescript/evitando-tipagens/Evitando-tipagens.ts
@@ -0,0 +1,35 @@
+function sendSpachip(spachip: {pilot: string, copilot?: string}) {
+  //...
+
+}
+
+sendSpachip({pilot : "Han Solo", copilot: "Rod"})
+
+sendSpachip({pilot:"Rodrigo"})
+
+
+let input: any
+
+input = "test"
+input = 20
+input = []
+
+let text: string
+
+text = input
+
+function verification(test) {
+  if (test) {
+
+  } else {
+    let _check:never
+
+    let text = _check
+
+    text = ""
+
+
+    return _check
+  }
+
+}
